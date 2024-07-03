e318b315434a193f19cfcaf278686fbf6eb1fedd
Stefan Claas <stefan.claas@mail.ru>
2024-07-03 21:33:34 +0200

    Add files via upload

--- v.sh
+++ v.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-# Usage: $ v '<20240701202947.4A5V43021BCW@sewer.dizum.com>' pubkey_file
-# For a test save my pub key 43e6681c5cf3cdc2f9ccf975f8a01b18c5e84bf0ba00605faba9cc0f8757a117
-# in a file named pubkey, without any LF or CRLF.
+# Usenet server details
+server="news.mixmin.net"
+port=119
 
-# Check if the correct number of arguments was passed
-if [ "$#" -ne 2 ]; then
-    echo "Please provide the Message-ID and the pubkey as arguments."
-    exit 1
-fi
-
-# Assign the arguments to variables
-message_id=$1
+msgid=$1
 pubkey=$2
 
-# Check if the Message-ID contains angle brackets
-if [[ $message_id != \<*\> ]]; then
-    message_id="<$message_id>"
+# Check if the Message-ID is enclosed in angle brackets
+if [[ $msgid != \<*\> ]]; then
+    # If not, add them
+    msgid="<"$msgid">"
 fi
 
-# Execute the command
-printf '%s\r\n' "article $message_id" quit . | nc news.mixmin.net 119 | awk '/^220 /{h=1;next}/^.\r$/{h=0;next}/^..\r$/{print".";next}h' | sve v $pubkey
+# Use netcat to connect to the server and get the article
+printf '%s\r\n' "article $msgid" quit . | nc $server $port\
+| sed -e :a -e '$d;N;2,2ba' -e 'P;D' | sve v $pubkey
+
+
+
