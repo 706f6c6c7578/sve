ba15c2e23dfac24a1c359d09de585d260ff10fbd
Stefan Claas <stefan.claas@mail.ru>
2024-07-13 10:06:17 +0200

    Update v.sh

--- v.sh
+++ v.sh
@@ -7,7 +7,6 @@ server="news.mixmin.net"
 port=119
 
 msgid=$1
-pubkey=$2
 
 # Check if the Message-ID is enclosed in angle brackets
 if [[ $msgid != \<*\> ]]; then
@@ -17,7 +16,7 @@ fi
 
 # Use netcat to connect to the server and get the article
 printf '%s\r\n' "article $msgid" quit | nc $server $port\
-| sed -e :a -e '$d;N;2,2ba' -e 'P;D' | sve v $pubkey
+| sed -e :a -e '$d;N;2,2ba' -e 'P;D' | sve v
 
 
 
